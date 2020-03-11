7882f1392f3f5cfeb17d6a3a96a77ad59d2c00e2
omair <captomair@gmail.com>
2020-03-11 07:51:01 +0500

    footer completed

--- file.js
+++ file.js
@@ -59,16 +59,31 @@ $('.gallery-responsive').slick({
     ]
 });
 
-
-
-// $('.counter').counterUp({
-//     delay: 10,
-//     time: 1000
-// });
-
+// FOR COUNTER
 $('.counter').countUp(
     {
         delay:100,
         time:1500
     }
 );
+
+// FOR SCROLL TO TOP BUTTON
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
